da4d182d216fd47de01143eab81b768cea6d290e
camilomoraux <124800178+camilomoraux@users.noreply.github.com>
2025-10-20 18:55:21 +0000

    Nueva página Player

--- src/router.jsx
+++ src/router.jsx
@@ -3,10 +3,12 @@ import Home from "@/pages/home";
 import PokeApi from "@/pages/poke-api";
 import NotFound from "@/pages/not-found";
 import Counter from "@/pages/counter";
+import Player from "@/pages/player";
 
 export const router = createBrowserRouter([
     { path: "/", element: <Home /> },
     { path: "/poke-api", element: <PokeApi /> },
     { path: "/counter", element: <Counter /> },
     { path: "*", element: <NotFound /> },
+    { path: "/player", element: <Player /> },
 ]);
